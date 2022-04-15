884a0c0c99b37bf246cb0cdf3dd9611157a7aa51
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-15 16:45:45 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,8 @@
 /**
  * _islower - checks for lowercase character
  *
+ * @c - Char to be checked
+ *
  * Return: Always 0 (Success)
  */
 
@@ -10,11 +12,11 @@ int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 
 }
