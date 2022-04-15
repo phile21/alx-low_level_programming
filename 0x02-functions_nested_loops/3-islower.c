54ceae88dcae49c742e42f438c0d9ecc29f833d7
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-15 05:28:40 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,20 +6,6 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
-	int r;
-	
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
-
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
