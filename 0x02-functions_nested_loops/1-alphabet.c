036770810688fa8df1958a7c67d65b621fea9dc5
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-15 03:52:22 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print_alphabet - print alphabets in lower case
+ *
+ * Return: Always 0 (Successs)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int a_z = 97;
+
+	while (a_z <= 122)
+	{
+		putchar(a_z);
+		a_z++;
+	}
+	putchar('\n');
+}
+
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 _putchar - A C program that prints _putchar, followed by a new line
+
+I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game - A C function containing a  function that prints the alphabet, in lowercase.
