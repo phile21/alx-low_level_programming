173eb59c181ff0b7bff3cab486ae44d1fcbe339b
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-15 17:14:48 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * int _isalpha(int c) - checks for alphabetic characters
+ *
+ * @c: parameter to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
