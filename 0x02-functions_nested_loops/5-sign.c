948748209d1acbea18b236ceb86228b6e1350a91
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-15 17:46:40 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,4 +16,14 @@ int print_sign(int n)
 		putchar('+');
 		return (1);
 	}
-}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+
