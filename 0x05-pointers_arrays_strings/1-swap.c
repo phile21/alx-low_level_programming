89d1aa2daccd91ddca4043e453752c4e2cdab62d
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-20 08:54:26 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swipes values of integers
+ *
+ * @a: parameter one
+ *
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a
+	*a = *b;
+	*b = tmp;
+}
+
