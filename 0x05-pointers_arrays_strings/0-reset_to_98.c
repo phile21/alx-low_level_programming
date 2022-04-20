6989412e9ed1395cdeb6af6ac9ddc6b20378be2a
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-20 08:47:02 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -updates a value the pointer points to
+ *
+ * @*n: parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+#include <stdio.h>
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+#endif
