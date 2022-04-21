4d9d964f019bbd6be05fa09113d0a130f7d0d728
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-21 03:48:12 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line
+ *
+ * @str: parameter holding the string 
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
