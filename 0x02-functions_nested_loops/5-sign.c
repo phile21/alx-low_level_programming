d3cc5af931b50cd470e4ec54a10f2948e050db71
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-24 05:04:55 -0700

    fixes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		return (1);
+		_putchar('+');
 	}
-	else if (n == 0)
-	{
-		putchar('0');
-		return (0);
-	}
-	else if (n < 0)
-	{
-		putchar('-');
-	}
-	return (-1);
+	return (1);
 }
