afb7938c1d04cced1a9ef48e563ae3375e6524f6
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-24 06:36:44 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,24 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	while (n <= 98)
 	{
-		while ( n < 98)
+		printf("%d", n);
+		if (n < 98)
 		{
-			printf("%d, ", n++);
-			printf("%d\n", n);
+			printf(", ");
 		}
+		n++;
 	}
-	else
+
+	while(n >= 98)
 	{
-		while (n > 98)
+		printf("%d", n);
+		if (n != 98)
 		{
-			printf("%d, ", n--);
-			printf("%d\n", n);
+			printf(", ");
 		}
+		n--;
 	}
+	printf("\n");
 }
