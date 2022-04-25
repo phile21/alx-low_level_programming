fa9d32ca0bba6633fb05d537a09adbf70e3b4513
Nkosingiphile <isabelnkocy1@gmail.com>
2022-04-25 03:26:58 -0700

    task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fibonacci_numbers();
+	return (0);
+}
+
+/**
+ * fibonacci_numbers - prints first 50 fibonacci numbers
+ *
+ * Return: returns nothing
+ */
+
+void fibonacci_numbers(void)
+{
+	int i = 3;
+
+	int frstnum = 1;
+
+	int secnum = 1;
+	
+	int next num;
+
+	while (i <= 50)
+	{
+		printf("%d, ", nextnum);
+		frstnum = secnum;
+		secnum = nextnum;
+		nextnum = frstnum + secnum;
+		i++;
+	}
+	printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,4 +30,6 @@ void print_times_table(int n);
 
 void numbers(void);
 
+void fibonacci_numbers(void);
+
 #endif
