a0c0dfc3d849699706a90336eafb0511e9e523f7
Nkosingiphile <isabelnkocy1@gmail.com>
2022-05-01 19:29:17 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int nums = 0, times = 1;
+
+	while (times <= 10)
+	{
+		nums = 0;
+		while (nums <= 14)
+		{
+			if (nums >= 10)
+			{
+				_putchar(nums / 10 + '0');
+			}
+			_putchar(nums % 10 + '0');
+			nums++;
+		}
+		_putchar('\n');
+		times++;
+	}
+}	
