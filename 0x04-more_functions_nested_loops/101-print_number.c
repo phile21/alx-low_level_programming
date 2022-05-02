84e868987f6dd5849d067b99588e0ad728ea6874
Nkosingiphile <isabelnkocy1@gmail.com>
2022-05-02 01:27:08 -0700

    task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: param holding integer
+ *
+ * Return: Always zero (Success)
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+}
