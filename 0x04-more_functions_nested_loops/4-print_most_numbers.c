2676996bb00ada28a590a9a7283adf53f6024fd0
Nkosingiphile <isabelnkocy1@gmail.com>
2022-05-01 18:33:07 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers but not including 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int nums = 48;
+
+	while ((nums <= 57) && (nums != 50) && (nums != 52))
+	{
+		_putchar(nums);
+		nums++;
+	}
+	_putchar('\n');
+}
