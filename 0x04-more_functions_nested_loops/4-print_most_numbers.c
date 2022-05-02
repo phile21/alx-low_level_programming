65e72a590eaefe9f61cb434377702753038c8527
Nkosingiphile <isabelnkocy1@gmail.com>
2022-05-01 18:47:00 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,12 @@ void print_most_numbers(void)
 {
 	int nums = 48;
 
-	while ((nums <= 57) && (nums != 50) && (nums != 52))
+	while (nums <= 57)
 	{
-		_putchar(nums);
+		if ((nums != 50) && (nums != 52))
+		{
+			_putchar(nums);
+		}
 		nums++;
 	}
 	_putchar('\n');
