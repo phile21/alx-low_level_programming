8e9f2252cb41fd787b6af22ac1af95c283b513a4
Nkosingiphile <isabelnkocy1@gmail.com>
2022-05-01 18:26:27 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int nums = 48;
+
+	while (nums <= 57)
+	{
+		_putchar(nums);
+		nums++;
+	}
+	_putchar('\n');
+}
