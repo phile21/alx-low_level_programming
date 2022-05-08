f2f8ee99dd71a3c2f0b527455d4724a8472eb02c
Nkosingiphile <isabelnkocy1@gmail.com>
2022-05-08 05:26:51 -0700

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,11 +20,11 @@ void print_98_fibonacci(void)
 {
 	int num1 = 1, num2 = 2, num3, num, count = 0;
 
+	num = 98;
 	printf("%d, ", num1);
 	printf("%d, ", num2);
 	count = 2;
-	num = 98;
-	while (count <= num)
+	while (count < num)
 	{
 		num3 = num1 + num2;
 		count++;
@@ -32,5 +32,4 @@ void print_98_fibonacci(void)
 		num1 = num2;
 		num2 = num3;
 	}
-	putchar('\n');
 }
